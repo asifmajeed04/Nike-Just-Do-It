c425de005e5a147152288527182aa75ec9c243b9
Asif Majeed <146339708+asifmajeed04@users.noreply.github.com>
2025-01-22 10:12:52 +0530

    index.html

---
+++ src/sections/index.js
@@ -0,0 +1,12 @@
+import Hero from './Hero';
+import CustomerReviews from './CustomerReviews';
+import Footer from './Footer';
+import PopularProducts from './PopularProducts';
+import Services from './Services';
+import SuperQuality from './SuperQuality';
+import SpecialOffer from './SpecialOffer';
+import Subscribe from './Subscribe';
+
+export{
+    Hero,PopularProducts,SuperQuality,Services,Subscribe,CustomerReviews,Footer,SpecialOffer
+}
